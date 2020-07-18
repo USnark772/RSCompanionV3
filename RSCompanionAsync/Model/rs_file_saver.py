651b66176ae02b60ca3e3bb8391f71c0eb5db633
Snark <phillipriskin@gmail.com>
2020-07-18 17:40:38 -0600

    More work on master output files.

--- RSCompanionAsync/Model/rs_file_saver.py
+++ RSCompanionAsync/Model/rs_file_saver.py
@@ -25,6 +25,8 @@ https://redscientific.com/index.html
 
 import os
 import tempfile
+from collections import defaultdict
+from operator import itemgetter
 from pathlib import Path
 from shutil import move
 
@@ -33,12 +35,13 @@ Take two directories, manipulate all data inside first directory as needed, then
 """
 
 
-def print_csv_dict(name: str, to_print: dict):
-    print(name + ":")
-    for key in to_print:
-        print(key)
-        for item in to_print[key]:
-            print("\t" + str(item))
+app_data_names = ["flags", "notes", "times"]
+data_filetype = ".csv"
+underscore_sep = "_"
+comma_sep = ","
+new_line = "\n"
+ts_hdr_name = 'timestamp'
+dev_id_hdr = "device id"
 
 
 class RSSaver:
@@ -56,98 +59,13 @@ class RSSaver:
         self._from_dir = tempfile.TemporaryDirectory()
         return self._from_dir.name + "/"
 
-    def stop(self) -> bool:
+    def stop(self) -> None:
         """
         Save data saved in temporary directory into desired output path.
         :return bool: Whether saving was successful.
         """
-        ret = False
-        self._alter_data()
+        self._finalize_data_output()
         self._move_data_to_output_dir()
-        return ret
-
-    def _alter_data(self) -> None:
-        """
-        Alter data as desired.
-        :return None:
-        """
-        drts = dict()
-        vogs = dict()
-        flags = dict()
-        notes = dict()
-        events = dict()
-        last_cond_name = str()
-        last_block_num = str()
-        last_flag = str()
-        os.chdir("C:/Users/phill/Companion Save Files/experiment_2020-07-18-14-06-12")
-        for file in os.listdir():
-            if file.endswith(".csv"):
-                print("Looking at file:", file)
-                info = file.rstrip(".csv").split("_")
-                if info[0] == "flags":
-                    pass
-                    flags = self._parse_csv_file(file)
-                    # print_csv_dict("flags_dict", flags)
-                elif info[0] == "notes":
-                    pass
-                    notes = self._parse_csv_file(file)
-                    # print_csv_dict("notes_dict", notes)
-                elif info[0] == "times":
-                    pass
-                    events = self._parse_csv_file(file)
-                    # print_csv_dict("events_dict", events)
-                elif info[0] == "DRT":
-                    temp = self._parse_csv_file(file, info[0] + "_" + info[1])
-                    self._merge_dicts(drts, temp)
-                    # print_csv_dict("devs_dict", devs)
-                elif info[0] == "VOG":
-                    temp = self._parse_csv_file(file, info[0] + "_" + info[1])
-                    self._merge_dicts(vogs, temp)
-        # print_csv_dict("drts_dict", drts)
-        # print_csv_dict("vogs_dict", vogs)
-        # TODO: Sort arrays in devs based on timestamp array.
-
-    @staticmethod
-    def _parse_csv_file(filename: str, dev_id: str = None) -> dict:
-        ret = dict()
-        dev = False
-        with open(filename) as f:
-            hdr = f.readline()
-            if dev_id is not None:
-                hdr_values = ["Device ID"]
-                more_vals = hdr.rstrip("\n").split(",")
-                for x in more_vals:
-                    hdr_values.append(x)
-                dev = True
-            else:
-                hdr_values = hdr.rstrip("\n").split(",")
-            print(hdr_values)
-            for value in hdr_values:
-                ret[value] = list()
-            for line in f:
-                line_values = line.rstrip("\n").split(",")
-                for i in range(len(hdr_values)):
-                    if dev and i == 0:
-                        ret[hdr_values[i]].append(dev_id)
-                    else:
-                        ret[hdr_values[i]].append(line_values[i-1])
-        return ret
-
-    @staticmethod
-    def _merge_dicts(a: dict, b: dict) -> dict:
-        """
-        Merge dict b into dict a. Assume both dictionaries have same structure or a is empty.
-        :param a: The data to add to.
-        :param b: The data to add.
-        :return dict: The resulting merge.
-        """
-        ret = dict()
-        print("a*********************************\n", a, "\n")
-        print("b*********************************\n", b, "\n")
-        for key in b:
-            ret[key] = b[key]
-        print("ret*******************************\n", ret, "\n")
-        return ret
 
     def _move_data_to_output_dir(self) -> None:
         """
@@ -160,16 +78,98 @@ class RSSaver:
         #         zipper.write(self._temp_folder.name + "/" + file, file)
         if not os.path.isdir(self._to_dir):
             os.mkdir(self._to_dir)
-        cur_dur = os.getcwd()
+        prev_dir = os.getcwd()
         os.chdir(self._from_dir.name)
-        for file in os.listdir(self._from_dir.name):
+        for file in os.listdir():
             move(file, self._to_dir)
-        os.chdir(cur_dur)
+        os.chdir(prev_dir)
+
+    def _finalize_data_output(self) -> None:
+        """
+        Alter data as desired.
+        :return None:
+        """
+        data: dict = self._parse_experiment()
+        for key in data.keys():
+            print("looking at data type: " + key + ".")
+            if key in app_data_names:
+                print("It's an app data type")
+            else:
+                print("It's a device.")
+
+    def _parse_experiment(self) -> dict:
+        data = dict()
+        num_devices = dict()
+        prev_dir = os.getcwd()
+        os.chdir(self._from_dir)  # TODO: Change this to self._from_dir.name when running with tempfile.
+        # os.chdir(self._from_dir.name)
+        for file in os.listdir():
+            if file.endswith(data_filetype):
+                info = file.rstrip(data_filetype).split(underscore_sep)
+                if info[0] in app_data_names:
+                    hdr, vals = self._parse_csv_file(file)
+                    data[info[0]] = [hdr, vals]
+                else:
+                    if info[0] not in num_devices.keys():
+                        num_devices[info[0]] = 0
+                    num_devices[info[0]] += 1
+                    hdr, vals = self._parse_csv_file(file, info[0] + underscore_sep + info[1])
+                    if info[0] not in data.keys():
+                        data[info[0]] = [hdr, []]
+                        data[info[0]][1].append(vals)
+                    else:
+                        data[info[0]][1].append(vals)
+        os.chdir(prev_dir)
+        for data_type in data:
+            if data_type in app_data_names:
+                continue
+            if num_devices[data_type] > 1:
+                data[data_type][1] = sorted([row for sublist in data[data_type][1] for row in sublist],
+                                            key=itemgetter(data[data_type][0][ts_hdr_name]))
+        return data
+
+    @staticmethod
+    def _parse_csv_file(filename: str, dev_id: str = None) -> (dict, list):
+        hdr_dict = dict()
+        rows = list()
+        dev = False
+        with open(filename) as f:
+            hdr = f.readline()
+            if dev_id is not None:
+                hdr_values = [dev_id_hdr]
+                more_vals = hdr.rstrip(new_line).split(comma_sep)
+                for x in more_vals:
+                    hdr_values.append(x)
+                dev = True
+            else:
+                hdr_values = hdr.rstrip(new_line).split(comma_sep)
+            print(hdr_values)
+            for i in range(len(hdr_values)):
+                hdr_dict[hdr_values[i]] = i
+            for line in f:
+                row = line.rstrip(new_line).split(comma_sep)
+                if dev:
+                    temp = [dev_id]
+                    for item in row:
+                        temp.append(item)
+                    row = temp
+                rows.append(row)
+        return hdr_dict, rows
 
 
 def main():
+    exp_dir = "C:/Users/phill/Companion Save Files/experiment_2020-07-18-14-06-12"
     saver = RSSaver()
-    saver._alter_data()
+    saver._from_dir = exp_dir
+    saver._finalize_data_output()
+    # hdr = {app_timestamp_hdr_name: 0, 'data': 1}
+    # values = [[(5, 1), (4, 5), (2, 1)], [(3, 2), (2, 1), (8, 4)], [(1, 8), (2, 6), (3, 0)]]
+    # the_dictionary = {'DRT': [hdr, values]}
+    # print(the_dictionary)
+    # for key in the_dictionary:
+    #     the_dictionary[key][1] = sorted([row for sublist in the_dictionary[key][1] for row in sublist],
+    #                                     key=itemgetter(the_dictionary[key][0][app_timestamp_hdr_name]))
+    # print(the_dictionary)
 
 
 if __name__ == '__main__':
